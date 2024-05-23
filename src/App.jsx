a48f35a1a95f29273d457b79ceaf0ecd8d9fbb4d
Yona Mahundi <yona101992@gmail.com>
2024-05-23 03:40:13 +0300

    fixed initial navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.jsx";
 import Buy from "./pages/Buy.jsx";
 import StockOnHand from "./pages/StockOnHand.jsx";
@@ -14,43 +13,40 @@ import BuySuccess from "./pages/BuySuccess.jsx";
 import ReportDesigner from "./pages/ReportDesigner.jsx";
 import General from "./pages/General.jsx";
 import Login from "./pages/Login.jsx";
-import {AuthProvider} from "./Providers/AuthProvider.jsx";
+import { AuthProvider } from "./Providers/AuthProvider.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 
 export default function App() {
   return (
-
-      <BrowserRouter>
+    <BrowserRouter>
       <AuthProvider>
         <Routes>
+          <Route path="/" element={<Navigate to="login" />} />
           <Route path="/login" element={<Login />} />
-
           <Route element={<PrivateRoute />}>
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="sell" element={<Sell />} />
-              <Route path="buy" element={<Buy />} />
-              <Route path="tx-success" element={<BuySuccess />} />
-              <Route path="expense" element={<Expense />} />
-              <Route path="reports" element={<Reports />} />
-              <Route path="stock-on-hand" element={<StockOnHand />} />
-              <Route
-                  path="stock-on-hand/adjustment/:id"
-                  element={<StockAdjustment />}
-              />
-              <Route path="settings/units" element={<Unit />} />
-              <Route path="settings/products" element={<Product />} />
-              <Route path="settings/general" element={<General />} />
-              <Route
-                  path="settings/report-designer"
-                  element={<ReportDesigner />}
-              />
-
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="sell" element={<Sell />} />
+            <Route path="buy" element={<Buy />} />
+            <Route path="tx-success" element={<BuySuccess />} />
+            <Route path="expense" element={<Expense />} />
+            <Route path="reports" element={<Reports />} />
+            <Route path="stock-on-hand" element={<StockOnHand />} />
+            <Route
+              path="stock-on-hand/adjustment/:id"
+              element={<StockAdjustment />}
+            />
+            <Route path="settings/units" element={<Unit />} />
+            <Route path="settings/products" element={<Product />} />
+            <Route path="settings/general" element={<General />} />
+            <Route
+              path="settings/report-designer"
+              element={<ReportDesigner />}
+            />
           </Route>
 
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </AuthProvider>
-      </BrowserRouter>
-
+    </BrowserRouter>
   );
 }
